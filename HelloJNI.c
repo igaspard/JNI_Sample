39df79c1cc1dad89749c99eecea4f5516840c526
igaspard <syuyang@gmail.com>
2013-01-04 12:07:25 +0800

    Test for git commit with -a -v. See how it works.

--- HelloJNI.c
+++ HelloJNI.c
@@ -12,6 +12,6 @@ JNIEXPORT void JNICALL Java_HelloJNI_printString(JNIEnv *env, jobject obj, jstri
 
 JNIEXPORT void JNICALL Java_HelloJNI_printHello(JNIEnv *env, jobject obj)
 {
-    printf("Hello World at C!\n");
+    printf("Hello World at C\n");
     return;
 }
